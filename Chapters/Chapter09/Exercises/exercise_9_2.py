f6500a9fa0e0055ec0da8da3761ee3f935266c2e
KayKozaronek <kozaronek@gmail.com>
2022-02-09 18:04:21 +0100

    added exercise 9.2

--- Chapters/Chapter09/Exercises/exercise_9_2.py
+++ Chapters/Chapter09/Exercises/exercise_9_2.py
@@ -1,28 +1,66 @@
 import streamlit as st
 
 def show():    
-    st.write('### Exercise 3.1')
+    st.write('### Exercise 9.2')
     st.write("""
-             Devise three example tasks of your own that fit into the MDP framework,
-             identifying for each its states, actions, and rewards. 
-             Make the three examples as different from each other as possible. 
-             The framework is abstract and flexible and can be applied in many different ways. 
-             Stretch its limits in some way in at least one of your examples.
+             Why does $(9.17)$ define $(n + 1)^k$ distinct features for dimension $k$?
              """)
     
     with st.expander('See Solution'):
-        st.write('#### Example 1:')
-        st.write("""**Scenario:** \n 
-    Deciding when and how much to fish.""")
-        st.write("""**States:** \n 
-    Number of fish in a given region. (Discrete)""")
-        st.write("""**Actions:** \n
-    - Fish
-    - Don't fish
-    - Artificially increase fish population
+        st.write("""
+                 Let's recall the definitions:
+                 - $n$ = order of polynomial (e.g. maximum exponent)
+                 - $k$ = dimension of state space (e.g. a grid world would be $k = 2$ dimensional since it has a horizontal and vertical component)
+                 
+                 Let's look at an example to see why we need $(n+1)^k$ (instead of $n^k$) distinct features for k dimension:
+                 
+                 **Case 1 ($n = 1$ , $k = 2$):**
+                 - $k = 2$ means that we have a 2 dimensional space. This means that each state $s$ corresponds to 2 numbers (a 2 dimensional vector) $s_1, s_2$
+                 - our formula would tell us that we obtain $(n+1)^k = (1+1)^2 = 4$ distinct features
+                 - we can spell these out:
+                 
+                    | 1 | 2     | 3     | 4        | 5       |
+                    |---|-------|-------|----------|---------|
+                    | 1 | $s_1$ | $s_2$ | $s_1s_2$ | $s_1^2$ | 
+                 
+                """)
+        
+        st.write("") 
+         
+        st.write(""" 
+                 Now let's look at what happens if we increase n by 1
+                 
+                 **Case 2 ($n = 2$ , $k = 2$):**
+                 - our formula would tell us that we obtain $(n+1)^k = (2+1)^2 = 9$ distinct features
+                 - we can spell these out:
+                 
+                    | 1 | 2     | 3     | 4        | 5       | 6       | 7          | 8          | 9            |
+                    |---|-------|-------|----------|---------|---------|------------|------------|--------------|
+                    | 1 | $s_1$ | $s_2$ | $s_1s_2$ | $s_1^2$ | $s_2^2$ | $s_1^2s_2$ | $s_1s_2^2$ | $s_1^2s_2^2$ |
                  """)
-        st.write("""**Rewards:** \n
-    - Minus 10 for artificially increasing fish population 
-    - Minus 1 for not fishing
-    - Plus 1 for fishing
+        
+        st.write("") 
+         
+        st.write(""" 
+                 Notice what happens for the case of $n = 0$ 
+                 
+                 **Case 3 ($n = 0$ , $k = 2$):**
+                 - our formula would tell us that we obtain $(n+1)^k = (1)^2 = 1$ distinct features
+                                  
+                    | 1 |
+                    |---|
+                    | 1 |
+                 """)
+        
+        st.write("") 
+         
+        st.write(""" 
+                 For completion here's what happens if we change k to  $k = 3$ 
+                 
+                 **Case 4 ($n = 1$ , $k = 3$):**
+                 - our formula would tell us that we obtain $(n+1)^k = (1+1)^3 = 8$ distinct features
+                 
+                    | 1 | 2     | 3     | 4     | 5        | 6        | 7        | 8           |
+                    |---|-------|-------|-------|----------|----------|----------|-------------|
+                    | 1 | $s_1$ | $s_2$ | $s_3$ | $s_1s_2$ | $s_1s_3$ | $s_2s_3$ | $s_1s_2s_3$ | 
                  """)
